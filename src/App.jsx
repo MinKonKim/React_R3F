ee9255afcf965ed4f4d8026897abfa0f649760a8
민곤 <alsrhs98@gmail.com>
2024-10-15 11:24:08 +0900

    3Dcanvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable react/no-unknown-property */
+import { Canvas } from "@react-three/fiber";
 import "./App.css";
-
+import * as THREE from "three";
 function App() {
   return (
     <Canvas>
-      <mesh></mesh>
+      <directionalLight intensity={2} position={[3, 3, 3]} />
+      <mesh
+        rotation={[THREE.MathUtils.DEG2RAD(45), THREE.MathUtils.DEG2RAD(45)]}
+      >
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={"red"} />
+      </mesh>
     </Canvas>
   );
 }
