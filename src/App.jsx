4e54365fb12e27cacd8a58914bf204cd33129877
민곤 <alsrhs98@gmail.com>
2024-10-15 14:08:50 +0900

    1주차 숙제

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-/* eslint-disable react/no-unknown-property */
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
-import * as THREE from "three";
+import { OrbitControls } from "@react-three/drei";
+
 function App() {
   return (
-    <Canvas>
-      <directionalLight intensity={2} position={[3, 3, 3]} />
-      <mesh
-        rotation={[THREE.MathUtils.DEG2RAD(45), THREE.MathUtils.DEG2RAD(45)]}
-      >
+    <Canvas camera={{ position: [3, 3, 3] }}>
+      <directionalLight position={[1, 2, 3]} intensity={2} />
+      <ambientLight intensity={0.5} />
+      <OrbitControls />
+      <gridHelper />
+      <mesh rotation={[Math.PI / 4, 0, 0]} position={[1, 1, 1]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={"blue"} />
+      </mesh>
+      <mesh rotation={[Math.PI / 4, Math.PI / 4, 0]} position={[-1, 0.5, 0]}>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color={"red"} />
+      </mesh>
+      <mesh rotation={[Math.PI / 4, 0, Math.PI / 4]} position={[0, 0.5, 3]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={"green"} />
       </mesh>
     </Canvas>
   );
